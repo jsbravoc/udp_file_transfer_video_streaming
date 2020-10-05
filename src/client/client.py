2b03869d648f7a924ec8404949f025d4f0af6a9a
jdcamachod <42348376+jdcamachod@users.noreply.github.com>
2020-10-04 20:57:14 -0500

    a

--- src/client/client.py
+++ src/client/client.py
@@ -1,38 +1,46 @@
 
 # Importing libraries
 import socket
-import sys
 import os
-
-# Lets catch the 1st argument as server ip
-#if (len(sys.argv) > 1):
-    #ServerIp = sys.argv[1]
-#else:
-    #print("\n\n Run like \n python3 client.py < serverip address > \n\n")
-    #exit(1)
-ServerIp = '52.186.137.191'
+import tqdm
 
 
-# Now we can create socket object
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 # send 4096 bytes each time step
+
+
+# the ip address or hostname of the server, the receiver
+host = '52.186.137.191'
+# the port, let's use 5001
+port = 9898
+# the name of file we want to send, make sure it exists
+filename = "sample.txt"
+# get the file size
+filesize = os.path.getsize(filename)
+
+# create the client socket
 s = socket.socket()
 
-# Lets choose one port and connect to that port
-PORT = 9898
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
 
-# Lets connect to that port where server may be running
-s.connect((ServerIp, PORT))
+# send the filename and filesize
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
-# We can send file sample.txt
-file = open(os.path.join("sample.txt"), "rb")
-SendData = file.read(1024)
-
-
-while SendData:
-    # Now we can receive data from server
-    print("\n\n################## Below message is received from server ################## \n\n ", s.recv(1024).decode("utf-8"))
-    #Now send the content of sample.txt to server
-    s.send(SendData)
-    SendData = file.read(1024)      
-
-# Close the connection from client side
+# start sending the file
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+    for _ in progress:
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in 
+        # busy networks
+        s.sendall(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+# close the socket
 s.close()
--- src/server/server.py
+++ src/server/server.py
@@ -1,45 +1,59 @@
-﻿# Importing socket library 
-import socket
+﻿import socket
+import tqdm
+import os
 
-# Now we can create socket object
+# device's IP address
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 9898
+# receive 4096 bytes each time
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+# create the server socket
+# TCP socket
 s = socket.socket()
 
-# Lets choose one port and start listening on that port
-PORT = 9898
-print("\n Server is listing on port :", PORT, "\n")
+# bind the socket to our local address
+s.bind((SERVER_HOST, SERVER_PORT))
 
-# Now we need to bind to the above port at server side
-s.bind(('', PORT))
+# enabling our server to accept connections
+# 5 here is the number of unaccepted connections that
+# the system will allow before refusing new connections
+s.listen(25)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-# Now we will put server into listenig  mode 
-s.listen(10)
+# accept connection if there is any
+client_socket, address = s.accept() 
+# if below code is executed, that means the sender is connected
+print(f"[+] {address} is connected.")
 
-#Open one recv.txt file in write mode
-file = open("recv.txt", "wb") 
-print("\n Copied file name will be recv.txt at server side\n")
 
-# Now we do not know when client will concatct server so server should be listening contineously  
-while True:
-    # Now we can establish connection with clien
-    conn, addr = s.accept()
+# receive the file infos
+# receive using client socket, not server socket
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+# remove absolute path if there is
+filename = os.path.basename(filename)
+# convert to integer
+filesize = int(filesize)
 
-    # Send a hello message to client
-    msg = "\n\n|---------------------------------|\n Hi Client[IP address: "+ addr[0] + "], \n ֲֳ**Welcome to Server** \n -Server\n|---------------------------------|\n \n\n"    
-    conn.send(msg.encode())
-    
-    # Receive any data from client side
-    RecvData = conn.recv(1024)
-    while RecvData:
-        file.write(RecvData)
-        RecvData = conn.recv(1024)
+# start receiving the file from the socket
+# and writing to the file stream
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "wb") as f:
+    for _ in progress:
+        # read 1024 bytes from the socket (receive)
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:    
+            # nothing is received
+            # file transmitting is done
+            break
+        # write to the file the bytes we just received
+        f.write(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
 
-    # Close the file opened at server side once copy is completed
-    file.close()
-    print("\n File has been copied successfully \n")
-
-    # Close connection with client
-    conn.close()
-    print("\n Server closed the connection \n")
-
-    # Come out from the infinite while loop as the file has been copied from client.
-    break
+# close the client socket
+client_socket.close()
+# close the server socket
+s.close()
