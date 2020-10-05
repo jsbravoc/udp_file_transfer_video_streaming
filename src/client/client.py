56639f110a1f99a0504f7beda901cd4fca9f7a55
Juan Sebastián Bravo <js.bravo@uniandes.edu.co>
2020-10-05 16:15:44 -0500

    Ajustes

--- src/client/client.py
+++ src/client/client.py
@@ -3,7 +3,25 @@
 import socket
 import os
 import tqdm
+import logging
+from datetime import datetime
 
+if not os.path.exists(f"{os.getcwd()}/logs/"):
+    os.makedirs(f"{os.getcwd()}/logs/")
+
+if not os.path.exists(f"{os.getcwd()}/logs/tqdm"):
+    os.makedirs(f"{os.getcwd()}/logs/tqdm")
+
+
+LOGS_FILE = f"{os.getcwd()}\\logs\\{str.replace(str(datetime.now()),':','-')}.log"
+
+logging.basicConfig(handlers=[logging.FileHandler(filename=LOGS_FILE,
+                                                  encoding='utf-8', mode='a+')],
+                    format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
+                    datefmt="%F %A %T",
+                    level=logging.INFO)
+logger_progress = logging.getLogger("Progress")
+logger_tcp = logging.getLogger("TCP_Packets")
 
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096 # send 4096 bytes each time step
@@ -34,20 +52,20 @@ filename, filesize = received.split(SEPARATOR)
 filename = os.path.basename(filename)
 # convert to integer
 filesize = int(filesize)
-
-progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+received = 0
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
 with open(filename, "wb") as f:
     for _ in progress:
         # read 1024 bytes from the socket (receive)
         bytes_read = s.recv(BUFFER_SIZE)
-        if not bytes_read:    
-            # nothing is received
-            # file transmitting is done
+        if received == filesize or not bytes_read:
+            progress.n = filesize
+            progress.refresh()
+            logger_progress.info(str(progress))
+            s.close()
+            progress.close()
             break
         # write to the file the bytes we just received
         f.write(bytes_read)
         # update the progress bar
         progress.update(len(bytes_read))
-
-# close the socket
-s.close()
--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 ﻿import socket
-from tqdm.auto import tqdm
+import tqdm
 import os
 from _thread import *
 from multiprocessing.dummy import Pool as ThreadPool
@@ -8,6 +8,9 @@ import io
 import shutil
 import logging
 import os
+import sys
+import copy
+from sys import platform
 
 if not os.path.exists(f"{os.getcwd()}/logs/"):
     os.makedirs(f"{os.getcwd()}/logs/")
@@ -15,9 +18,16 @@ if not os.path.exists(f"{os.getcwd()}/logs/"):
 if not os.path.exists(f"{os.getcwd()}/logs/tqdm"):
     os.makedirs(f"{os.getcwd()}/logs/tqdm")
 
-logging.basicConfig(filename=f'{os.getcwd()}/logs/{str.replace(str(datetime.now()),":","-")}.log',
-                    format='%(levelname)s %(asctime)s :: %(message)s',
-                    level=logging.DEBUG)
+
+LOGS_FILE = f"{os.getcwd()}\\logs\\{str.replace(str(datetime.now()),':','-')}.log"
+
+logging.basicConfig(handlers=[logging.FileHandler(filename=LOGS_FILE,
+                                                  encoding='utf-8', mode='a+')],
+                    format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
+                    datefmt="%F %A %T",
+                    level=logging.INFO)
+logger_progress = logging.getLogger("Progress")
+logger_tcp = logging.getLogger("TCP_Packets")
 
 # CONSTANTS
 SERVER_HOST = "0.0.0.0"
@@ -26,8 +36,8 @@ BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 DEFAULT_DIRECTORY = "D:\\Downloads"
 
-LOGS_FILE = f"{os.getcwd()}\\logs\\tqdm\\{str.replace(str(datetime.now()),':','-')}.txt"
 STRING_BUFFER = io.StringIO("some initial text data")
+
 
 def read_listdir(dir):
     """
@@ -51,26 +61,29 @@ def read_listdir(dir):
 def threaded(client):
     client[0].send(f"{filename}{SEPARATOR}{filesize}".encode())
     # start sending the file
-    progress = tqdm(range(filesize), f"Sending {filename} to {client[1][0]}", unit="B", unit_scale=True,
-                    unit_divisor=BUFFER_SIZE, leave=False, file=STRING_BUFFER)
+    progress = tqdm.tqdm(range(filesize), f"Enviando {filename} a {client[1][0]}", unit="B", unit_scale=True,
+                unit_divisor=BUFFER_SIZE)
+    sended = 0
     with open(filename, "rb") as f:
         for _ in progress:
             # read the bytes from the file
             bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
+            if sended == filesize:
                 # file transmitting is done
+                progress.n = filesize
+                progress.refresh()
+                logger_progress.info(str(progress))
+                client[0].close()
+                progress.close()
                 break
             # we use sendall to assure transimission in
             # busy networks
             client[0].sendall(bytes_read)
             # update the progress bar
+            sended += len(bytes_read)
             progress.update(len(bytes_read))
-    # start receiving the file from the socket
-    # and writing to the file stream
-
-    # close the client socket
-    c.close()
-
+            logger_tcp.critical(f"Enviando {filename} a {client[1][0]} Tamaño paquete: {BUFFER_SIZE}, "
+                    f"paquete :{round(sended/BUFFER_SIZE)}/{round(filesize/BUFFER_SIZE)}" )
 
 print("** Para utilizar valores por defecto, ingresar cadena vacía **")
 directory = str(input(f"Ingrese la dirección del directorio (valor por defecto: {DEFAULT_DIRECTORY}): "))
@@ -91,12 +104,16 @@ print("-------------------------------------------------------------------------
 for l in lista:
     print(l)
 print("\n---------------------------------------------------------------------------")
-opt = int(input("Seleccione el archivo que quiere enviar: "))
-while opt > len(lista):
+opt = input("Seleccione el archivo que quiere enviar: ")
+while not opt.isnumeric() or int(opt) > len(lista):
     opt = input("Seleccione una opción válida: ")
+opt = int(opt)
 filename = os.path.join(directory, lista[opt - 1].partition("-")[len(lista[opt - 1].partition("-")) - 1])
 filesize = os.path.getsize(filename)
-usrs = int(input("Seleccione la cantidad de usuarios a los que quiere enviar el archivo: "))
+usrs = input("Seleccione la cantidad de usuarios a los que quiere enviar el archivo: ")
+while not usrs.isnumeric() or int(usrs) < 1:
+    usrs = input("Seleccione una opción válida: ")
+usrs = int(usrs)
 # accept connection if there is any
 conns = 0
 cmpltd = usrs
@@ -133,14 +150,12 @@ try:
     print(f"            Se finalizó la transferencia para los {usrs} usuarios           ")
     print("\n---------------------------------------------------------------------------")
     s.close()
-    print("\n---------------------------------------------------------------------------\n")
-    print(f"                            Abriendo logs           ")
-    print("\n---------------------------------------------------------------------------")
-    with open (LOGS_FILE, 'a') as fd:
-        STRING_BUFFER.seek (0)
-        shutil.copyfileobj (STRING_BUFFER, fd)
-    os.startfile(LOGS_FILE)
+    if platform == "linux" or platform == "linux2":
+        print(f"El archivo de logs se encuentra en:\n {LOGS_FILE}")
+    elif platform == "win32":
+        os.startfile(LOGS_FILE)
     exit()
+
 
 
 except KeyboardInterrupt:
